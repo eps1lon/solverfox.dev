1e1b84ebcdafb4103975baa8387b1a18f66fde5b
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-04 17:04:23 +0200

    chore: Add prettier

--- src/routes/blog/[slug].json.js
+++ src/routes/blog/[slug].json.js
@@ -12,17 +12,19 @@ export function get(req, res, next) {
 
 	if (lookup.has(slug)) {
 		res.writeHead(200, {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
 		});
 
 		res.end(lookup.get(slug));
 	} else {
 		res.writeHead(404, {
-			'Content-Type': 'application/json'
+			'Content-Type': 'application/json',
 		});
 
-		res.end(JSON.stringify({
-			message: `Not found`
-		}));
+		res.end(
+			JSON.stringify({
+				message: `Not found`,
+			}),
+		);
 	}
 }
