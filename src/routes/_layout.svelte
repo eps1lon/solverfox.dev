912c02192ec50db765b39a2bc44a0913741802bf
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-10-13 21:48:00 +0200

    feat: Misc a11y improvements (#6)

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -13,11 +13,53 @@
 		margin: 0 auto;
 		box-sizing: border-box;
 	}
+
+	/* based on https://www.w3.org/TR/WCAG20-TECHS/slicenav.css from 2019-10-13 */
+	.skipnav {
+		position: absolute;
+		text-align: left;
+		margin: 0;
+		padding: 0;
+	}
+
+	.skipnav a {
+		width: 26em;
+		display: block;
+		color: #fff;
+		background: #333;
+		text-decoration: none;
+		padding: 5px;
+		position: absolute;
+		left: -1000em;
+		top: 0;
+		font-weight: bold;
+	}
+	.skipnav a:visited {
+		color: #fff;
+	}
+	.skipnav a:focus,
+	.skipnav a:active {
+		z-index: 99;
+		top: 1em;
+		left: 1em;
+		color: #fff;
+		background: #333 !important;
+	}
+
+	.skipnav a:hover {
+		cursor: default;
+	}
 </style>
+
+<div id="skipnav">
+	<p class="skipnav">
+		<a href="#maincontent">Skip to main content (Press Enter)</a>
+	</p>
+</div>
 
 <Nav {segment} />
 
-<main>
+<main id="maincontent">
 	<slot />
 </main>
 
