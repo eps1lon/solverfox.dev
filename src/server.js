b8d0a387f9d3985f30a576612d6d87a67962752d
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-09 22:24:58 +0200

    explicitly use n_m for sapper files

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import sirv from 'sirv';
 import polka from 'polka';
 import compression from 'compression';
-import * as sapper from '@sapper/server';
+import * as sapper from './node_modules/@sapper/server';
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === 'development';
