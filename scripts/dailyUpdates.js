9a375ec4569e660715be466a239c5281e80977d2
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-15 23:06:06 +0200

    Update contributions daily

---
+++ scripts/dailyUpdates.js
@@ -0,0 +1,50 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const childProcess = require('child_process');
+const { promisify } = require('util');
+
+const exec = promisify(childProcess.exec);
+
+async function git(command, ...args) {
+	const { stdout, stderr } = await exec(`git ${command}`, ...args);
+	if (stdout) core.debug(stdout);
+	return { stdout, stderr };
+}
+
+async function main() {
+	await exec('node scripts/loadContributions static/contributions.json');
+	await exec('yarn format');
+
+	const { stdout: gotUpdated } = await git('status --porcelain');
+
+	if (gotUpdated) {
+		await git('config --local user.email "action@github.com"');
+		await git('config --local user.name "GitHub Action"');
+
+		const branch = `github-actions/daily-updates`;
+		await git(`checkout -b ${branch}`);
+		await git('add -A');
+		await git('commit -m "Daily updates"');
+
+		await git(`push origin -f ${branch}`);
+		const octokit = new github.GitHub(core.getInput('token'));
+		try {
+			await octokit.pulls.create({
+				owner: github.context.repo.owner,
+				repo: github.context.repo.repo,
+				base: 'master',
+				head: branch,
+				title: `Daily updates`,
+				body: 'Automatically generated',
+				maintainer_can_modify: true,
+			});
+		} catch (error) {
+			core.warning(`'${JSON.stringify(error, null, 2)}'`);
+		}
+	}
+}
+
+main().catch((error) => {
+	console.error(error);
+	process.exit(1);
+});
