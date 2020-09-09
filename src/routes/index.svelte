c03e1e95446c161ad2628a3b28eae33bb3ec4f34
Christopher Hogg <cthogg@users.noreply.github.com>
2020-09-09 11:17:48 +0200

    docs: Fix typo (#63)

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -53,7 +53,7 @@
 <p>
 	Hey there! My name is
 	<strong>Sebastian</strong>
-	and I'm a frontent developer with a focus on accessibility and developer
+	and I'm a frontend developer with a focus on accessibility and developer
 	tooling. I'm a member of the core team working on
 	<a href="https://material-ui.com">Material-UI</a>
 	and member of
